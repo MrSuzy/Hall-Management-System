913ed649b3b77c5c41cf3ef8f3a654d41aa907ed
MrSuzy <szhixuen@gmail.com>
2024-09-17 21:59:44 +0800

    user authentication

---
+++ src/Customer/bookingClass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Customer;
+
+import java.util.Date;
+
+/**
+ *
+ * @author xuen_
+ */
+public class bookingClass {
+    private String bookingID;
+    private String custID;
+    private String hallID;
+    private Date bookingDate;
+    private String paymentStatus;
+    
+    
+}
--- src/Customer/customerClass.java
+++ src/Customer/customerClass.java
@@ -4,38 +4,34 @@
  */
 package Customer;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author xuen_
  */
 public class customerClass {
-    private String custID;
     private String name;
-    private String phoneNum;
     private String email;
     private String address;
     private String password;
+    private String role;
     
-    public customerClass(String custID, String name, String phoneNum, String email, String address, String password) {
-        this.custID = custID;
+    public customerClass(String name, String email, String address, String password, String role) {
         this.name = name;
-        this.phoneNum = phoneNum;
         this.email = email;
         this.address = address;
         this.password = password;
+        this.role = role;
     }
     
     // getters method
-    public String getCustID() {
-        return custID;
-    }
-    
     public String getName() {
         return name;
-    }
-    
-    public String getPhoneNum() {
-        return phoneNum;
     }
     
     public String getAddress() {
@@ -46,11 +42,11 @@ public class customerClass {
         return password;
     }
     
-    // setters method
-    public void setCustID(String custID) {
-        this.custID = custID;
+    public String getRole() {
+        return role;
     }
     
+    // setters method  
     public void setName(String name) {
         this.name = name;
     }
@@ -67,9 +63,37 @@ public class customerClass {
         this.password = password;
     }
     
-    // update profile method
-    public void updateProfile(String address, String password) {
-        this.address = address;
-        this.password = password;
+    public void setRole(String role) {
+        this.role = role;
     }
+    
+    // update to text file 
+    public void customerRecord() {
+        try{
+            FileWriter fw = new FileWriter("users.txt", true);
+            fw.write(name + "," + address + "," + email + "," + password + "," + role + "\n");
+        } catch (IOException e) {
+            System.out.println("Exception error." + e.getMessage());
+
+        }
+    }
+    
+    // user authentication method 
+    public static customerClass authenticate(String email, String password) {
+        try{
+            FileReader fr = new FileReader("users.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String read;
+            while ((read = br.readLine()) != null) {
+                String[] details = read.split(",");
+                if(details[3].equals(email) && details[4].equals(password)) {
+                    return new customerClass(details[0], details[1], details[2], details[3], details[4]);
+                }
+            }           
+        } catch (IOException e) {
+            System.out.println("Exception error." + e.getMessage());
+        }
+        return null;
+    }
+    
 }
