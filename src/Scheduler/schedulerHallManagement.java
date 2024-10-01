c081898a0ce7a33d67c268374d08544bd1c7420d
MrSuzy <szhixuen@gmail.com>
2024-10-01 22:42:12 +0800

    added view hall details for scheduler debug done

--- src/Scheduler/schedulerHallManagement.java
+++ src/Scheduler/schedulerHallManagement.java
@@ -75,7 +75,6 @@ public class schedulerHallManagement extends javax.swing.JFrame {
         btnSearchInHallTable = new javax.swing.JButton();
         btnResetSearch = new javax.swing.JButton();
         btnViewHallDetails = new javax.swing.JButton();
-        btnViewPast = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(227, 242, 253));
@@ -200,14 +199,6 @@ public class schedulerHallManagement extends javax.swing.JFrame {
             }
         });
 
-        btnViewPast.setFont(new java.awt.Font("Gill Sans MT", 0, 18)); // NOI18N
-        btnViewPast.setText("Past");
-        btnViewPast.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnViewPastActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -217,13 +208,11 @@ public class schedulerHallManagement extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(btnEdit, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(btnDelete, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(btnAdd, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(btnViewHallDetails, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(btnViewPast, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(btnEdit, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnDelete, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnAdd, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnViewHallDetails, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(67, 67, 67)
@@ -265,7 +254,7 @@ public class schedulerHallManagement extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                 .addComponent(btnAdd)
                                 .addGap(18, 18, 18)
                                 .addComponent(btnEdit)
@@ -273,8 +262,7 @@ public class schedulerHallManagement extends javax.swing.JFrame {
                                 .addComponent(btnDelete)
                                 .addGap(18, 18, 18)
                                 .addComponent(btnViewHallDetails)
-                                .addGap(18, 18, 18)
-                                .addComponent(btnViewPast)))))
+                                .addGap(44, 44, 44)))))
                 .addGap(16, 16, 16))
         );
 
@@ -387,7 +375,7 @@ public class schedulerHallManagement extends javax.swing.JFrame {
     private void btnViewHallDetailsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnViewHallDetailsActionPerformed
         int selectedRow = tableHallManagement.getSelectedRow();
         
-        if (selectedRow > 0 ) {
+        if (selectedRow >= 0 ) {
             String hallID = tableHallManagement.getValueAt(selectedRow, 0).toString();
             
             this.dispose();
@@ -396,21 +384,6 @@ public class schedulerHallManagement extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, "No hall selected. Please select a hall to view details.", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }//GEN-LAST:event_btnViewHallDetailsActionPerformed
-
-    private void btnViewPastActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnViewPastActionPerformed
-        // TODO add your handling code here:
-        hallClass hall = new hallClass();
-        
-        List<Object[]> past = hall.viewPastHalls();
-        
-        DefaultTableModel table = (DefaultTableModel) tableHallManagement.getModel();
-        
-        table.setRowCount(0);
-        
-        for (Object[] row : past) {
-            table.addRow(row);
-        }
-    }//GEN-LAST:event_btnViewPastActionPerformed
 
     /**
      * @param args the command line arguments
@@ -427,7 +400,6 @@ public class schedulerHallManagement extends javax.swing.JFrame {
     private javax.swing.JButton btnResetSearch;
     private javax.swing.JButton btnSearchInHallTable;
     private javax.swing.JButton btnViewHallDetails;
-    private javax.swing.JButton btnViewPast;
     private javax.swing.JFileChooser jFileChooser1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
